71fb151bf18597a861adb34c45b08eeebf81ba97
Eben80 <sbmail246@gmail.com>
2025-08-03 23:12:53 +0000

    db log files

--- db/db_config.php
+++ db/db_config.php
@@ -15,8 +15,8 @@ $conn->set_charset("utf8mb4");  // IMPORTANT: Set charset to utf8mb4 here
 
 // --- Debug Logging Function ---
 function debug_log($message) {
-    $log_file = __DIR__ . '/../logs/php_debug.log';
-    $timestamp = date('Y-m-d H:i:s');
+    $log_file = __DIR__ . '/logs/php_debug.log';
+   $timestamp = date('Y-m-d H:i:s');
     $log_message = "[$timestamp] " . print_r($message, true) . "\n";
     file_put_contents($log_file, $log_message, FILE_APPEND);
 }
