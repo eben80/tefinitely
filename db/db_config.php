1e8894713d92bedbd2ac22fb2ae29292939caa3f
eben80 <sbmail246@gmail.com>
2025-08-07 15:56:06 +0200

    fix

--- db/db_config.php
+++ db/db_config.php
@@ -14,10 +14,10 @@ if ($conn->connect_error) {
 $conn->set_charset("utf8mb4");  // IMPORTANT: Set charset to utf8mb4 here
 
 // --- Debug Logging Function ---
-function debug_log($message) {
-    $log_file = __DIR__ . '/logs/php_debug.log';
-   $timestamp = date('Y-m-d H:i:s');
-    $log_message = "[$timestamp] " . print_r($message, true) . "\n";
-    file_put_contents($log_file, $log_message, FILE_APPEND);
-}
+// function debug_log($message) {
+//     $log_file = __DIR__ . '/logs/php_debug.log';
+//    $timestamp = date('Y-m-d H:i:s');
+//     $log_message = "[$timestamp] " . print_r($message, true) . "\n";
+//     file_put_contents($log_file, $log_message, FILE_APPEND);
+// }
 ?>
