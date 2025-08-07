2e2b2c32cf9cda1fe499c0a5cf7cf6c1c656eb2c
eben80 <sbmail246@gmail.com>
2025-08-07 19:38:29 +0200

    Update setup_database.php

--- db/setup_database.php
+++ db/setup_database.php
@@ -3,10 +3,10 @@ require_once 'db_config.php';
 
 // SQL files to execute
 $sql_files = [
-    __DIR__ . '/../sql/create_phrases_table.sql',
-    __DIR__ . '/../sql/create_user_management_tables.sql',
-    __DIR__ . '/../sql/create_password_reset_table.sql',
-    __DIR__ . '/../sql/update_user_tracking.sql',
+    // __DIR__ . '/../sql/create_phrases_table.sql',
+    // __DIR__ . '/../sql/create_user_management_tables.sql',
+    // __DIR__ . '/../sql/create_password_reset_table.sql',
+    // __DIR__ . '/../sql/update_user_tracking.sql',
     __DIR__ . '/../sql/create_training_tables.sql',
     __DIR__ . '/../sql/alter_roleplay_scenarios.sql',
     __DIR__ . '/../sql/update_roleplay_scenarios_level2.sql'
