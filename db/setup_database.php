968fae8f0b1173021399ec0597d46049a17026c5
eben80 <sbmail246@gmail.com>
2025-08-07 20:06:37 +0200

    Update setup_database.php

--- db/setup_database.php
+++ db/setup_database.php
@@ -9,7 +9,7 @@ $sql_files = [
     // __DIR__ . '/../sql/update_user_tracking.sql',
     // __DIR__ . '/../sql/create_training_tables.sql',
     // __DIR__ . '/../sql/alter_roleplay_scenarios.sql',
-    __DIR__ . '/../sql/update_roleplay_scenarios_level2.sql',
+    // __DIR__ . '/../sql/update_roleplay_scenarios_level2.sql',
     __DIR__ . '/../sql/add_more_drills.sql'
 ];
 
